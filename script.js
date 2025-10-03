4773f296727444078489ef773afb0c2922c6cb0d
RafaelS-Costa <rafaelsartoricosta@outlook.com>
2025-10-02 21:26:30 -0300

    inclusão script.js cadastro de ong no banco

---
+++ script.js
@@ -0,0 +1,22 @@
+//use 'onglinkDb';
+// console.log(
+// db.ongs.insertOne({
+//     "cad_ong":"00",
+//     "nome":"Ong teste",
+//     "endereco":"Rua do teste",
+//     "cnpj":"11.123.456/0001-00",
+//     "telefone": "15 3322-3322",
+//     "rep_legal":"João das Ongs",
+//     "cpf":"111.222.333-00",
+//     "email":"ong_email@email.com.br",
+//     "logo":"linkdologo123.com",
+//     "data_fundacao":"2025/09/02",
+//     "causa_social":"educação",
+//     "arquivos_legais":"linkdosarquivos123.com",
+//     "imagens":"linkdasimagens123.com"
+// })
+// );
+
+console.log(
+    db.ongs.find({})
+);
